99adda1c23cbb04e5a4f4b925292a56f9e8e3492
Sergio Castro <sergioc78@gmail.com>
2013-01-12 14:11:23 +0100

    the osm example (the MapQuery application) was moved to its own independent project

--- src/main/java/org/jpc/examples/osm/Taggeable.java
+++
@@ -1,9 +0,0 @@
-package org.jpc.examples.osm;
-
-import java.util.Map;
-
-public interface Taggeable {
-
-	public Map<String,String> getTags();
-
-}
--- src/test/java/org/jpc/examples/LogtalkExamplesTestSuite.java
+++ src/test/java/org/jpc/examples/LogtalkExamplesTestSuite.java
@@ -1,7 +1,6 @@
 package org.jpc.examples;
 
 import org.jpc.examples.metro.MetroTestSuite;
-import org.jpc.examples.osm.OsmTestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -13,6 +12,5 @@ import org.junit.runners.Suite.SuiteClasses;
  */
 @RunWith(Suite.class)
 @SuiteClasses({
-	MetroTestSuite.class, 
-	OsmTestSuite.class})
+	MetroTestSuite.class})
 public class LogtalkExamplesTestSuite {}
