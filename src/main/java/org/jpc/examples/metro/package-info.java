03748ef4b62597c15004eb6876ceb512161c6659
Sergio Castro <sergioc78@gmail.com>
2014-03-05 16:00:02 +0100

    fixing typo

--- src/main/java/org/jpc/examples/metro/package-info.java
+++ src/main/java/org/jpc/examples/metro/package-info.java
@@ -1,6 +1,6 @@
 /**
  * A JPC example based on the London Underground system.
- * Example adapted from the "Simply Logical" book of Peter Flach.
+ * Example adapted from the "Simply Logical" book by Peter Flach.
  * @author sergioc
  *
  */
